dd96a6d1af8ee8f5a7236dd616a2ae94fe706d00
Khang Huynh <kkyhuynh@gmail.com>
2025-04-17 14:47:56 -0500

    current weather of where I live!

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,56 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            var client = new HttpClient();
+
+            var kanyeUrl = "https://api.kanye.rest/";
+
+            var kanyeResponseJson = client.GetStringAsync(kanyeUrl).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponseJson).GetValue("quote").ToString();
+            
+            Console.WriteLine(kanyeQuote);
+
+
+
+            var appsettingsText = File.ReadAllText("appsettings.json");
+            
+            var apiKey = JObject.Parse(appsettingsText)["key"].ToString();
+
+            //Console.WriteLine("Please enter your zip code:");
+
+            //var zip = Console.ReadLine();
+
+           // var weatherUrl =
+                //$"https://api.openweathermap.org/data/2.5/weather?zip={}&appid={apiKey}"; 
+                
+                Console.WriteLine("What is the weather like today in Bentonville, AR?");
+                
+                //var weather = Console.ReadLine();
+                
+                var weatherUrl =
+                    $"https://api.openweathermap.org/data/2.5/weather?lat=36.372852&lon=-94.208817&appid={apiKey}&units=imperial";
+                
+                
+                var weatherResponseJson = client.GetStringAsync(weatherUrl).Result;
+                
+                
+                var weatherNumber = JObject.Parse(weatherResponseJson)["main"]["temp"].ToString();
+
+                var weatherTemp = Math.Round(double.Parse(weatherNumber));
+                
+                var weatherDescription = JObject.Parse(weatherResponseJson)["weather"][0]["description"].ToString();
+                
+                Console.WriteLine($"{weatherNumber} degrees Fahrenheit with {weatherDescription}!");
+                
+                
+
+
         }
     }
 }
